0710aa850f93815761682c049195f2964e5b8687
unknown <jadsonkenard@hotmail.comgit config --global user.email jadsonkenard@hotmail.com>
2024-04-17 11:56:34 -0300

    criação de token

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -16,6 +16,7 @@ export default function Login() {
         handleOnChangePassword,
     } = useLogin();
 
+
     return (
         <View>
             <ContainerLogin>
---
+++ src/shared/constants/urls.ts
@@ -0,0 +1,2 @@
+
+export const URL_USER = 'http://192.168.18.6:8080/user'
